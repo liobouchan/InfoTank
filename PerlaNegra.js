4e0cc305af639cb8bf4676f1e27621b5db3768f0
Liobouchan <leo.bouchan@gmail.com>
2014-06-27 14:16:01 -0500

    Returns the distance in two points

--- PerlaNegra.js
+++ PerlaNegra.js
@@ -1,32 +1,58 @@
   //Metodo inicial el cual es invocado una sola vez al iniciar el juego
-  function init(tank){
-      //Asigna el nombre del tanque
-      tank.setName("Perla Negra");
-  }
-
-  //Metodo de procesamiento del juego el cual es invocado cada 20 milisegundos
-  function move(tank){
-
-    //Se mueve una posición a adelante
-    if(!tank.moveForward()){//si no pudo moverse (llego a una esquina)            
-      //Cambia dirección en ángulo aleatorio
-      tank.setTankDirection(Math.random() * 360);
-      //Si hay teleportaciones disponibles, inicia teleportación...
-      if(tank.canFireTeleport())tank.fireTeleport(Math.random() * tank.getBattlefieldWidth(), Math.random() * tank.getBattlefieldHeight());
+    function init(tank){
+        //Asigna el nombre del tanque
+        tank.setName("Perla Negra");
     }
 
-    //Si tiene balas dispara en un ángulo aleatorio 
-    if(tank.canFireBullet())tank.fireBullet(Math.random() * 360);
+  //Regresa la distancia entre dos puntos
+    function getDist(x1, y1, x2, y2){
+      var a = y2 - y1;
+      var b = x2 - x1;
+      return Math.sqrt((a * a) + (b * b));
+    }
 
-    //Si tiene misiles dispara en ángulo y distancia aleatoria 
-    if(tank.canFireMissile())tank.fireMissile(Math.random() * 360,Math.random() * tank.getBattlefieldHeight());
+  //Regresa el tanque (TankInfo) más cercano
+    function getNearestTank(base){
 
-    //Si tiene minas disponibles dispara una mina en la posición actual 
-    if(tank.canFireMine())tank.fireMine();
+      var tanks = base.getTanksInfo().iterator();
+      var tank = null;
+      var dis;
 
-    //Si tiene campos de fuerza disponibles dispara un campo de fuerza
-    if(tank.canFireShield())tank.fireShield();
+      while (tanks.hasNext()){
+        var aux = tanks.next();
+        var d = getDist(base.getX(),base.getY(),aux.getX(),aux.getY());
+        if (d < dis || tank==null){
+          tank = aux;
+          dis = d;
+        }
+      }
 
-    //Disparar pulso electromagnético
-    if(tank.canFireEmp())tank.fireEmp(10);
-  }
+      return tank;
+    }
+
+  //Metodo de procesamiento del juego el cual es invocado cada 20 milisegundos
+    function move(tank){
+
+      //Se mueve una posición a adelante
+      if(!tank.moveForward()){//si no pudo moverse (llego a una esquina)            
+        //Cambia dirección en ángulo aleatorio
+        tank.setTankDirection(Math.random() * 360);
+        //Si hay teleportaciones disponibles, inicia teleportación...
+        if(tank.canFireTeleport())tank.fireTeleport(Math.random() * tank.getBattlefieldWidth(), Math.random() * tank.getBattlefieldHeight());
+      }
+
+      //Si tiene balas dispara en un ángulo aleatorio 
+      if(tank.canFireBullet())tank.fireBullet(Math.random() * 360);
+
+      //Si tiene misiles dispara en ángulo y distancia aleatoria 
+      if(tank.canFireMissile())tank.fireMissile(Math.random() * 360,Math.random() * tank.getBattlefieldHeight());
+
+      //Si tiene minas disponibles dispara una mina en la posición actual 
+      if(tank.canFireMine())tank.fireMine();
+
+      //Si tiene campos de fuerza disponibles dispara un campo de fuerza
+      if(tank.canFireShield())tank.fireShield();
+
+      //Disparar pulso electromagnético
+      if(tank.canFireEmp())tank.fireEmp(10);
+    }
